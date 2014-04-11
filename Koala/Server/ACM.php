ec0613f9cdf12bf47a2862f6a6ce7ace608f67c8
lunnlew <lunnlew@qq.com>
2014-04-11 23:55:28 +0800

    新增ACM,Cache,Collection详细注释

--- Koala/Server/ACM.php
+++ Koala/Server/ACM.php
@@ -1,28 +1,43 @@
 <?php
 /**
- * 
- *访问控制服务
+ * Koala - A PHP Framework For Web
  *
+ * @package  Koala
+ * @author   Lunnlew <Lunnlew@gmail.com>
+ */
+/**
+ * 访问控制服务类
+ * 
+ * @package  Koala
+ * @subpackage  Server
+ * @author    Lunnlew <Lunnlew@gmail.com>
  */
 class ACM{
   	/**
    	* 操作句柄数组
    	* @var array
+   	* @access protected
    	*/
-  	static protected $handlers = array();
-	public function __construct(){}
-	public static function factory($type='',$options=array()){
-		if(empty($type)||!is_string($type)){
-			$type = C('ACM:DEFAULT','ACM');
+  	protected static $handlers = array();
+  	/**
+  	 * 访问控制实例化函数
+  	 * 
+  	 * @param  string $name    驱动名
+  	 * @param  array  $options 驱动构造参数
+  	 * @return object          驱动实例
+  	 */
+	public static function factory($name='',$options=array()){
+		if(empty($name)||!is_string($name)){
+			$name = C('ACM:DEFAULT','ACM');
 		}
-		if(!isset(self::$handlers[$type])){
-			$c_options = C('ACM:'.$type);
+		if(!isset(self::$handlers[$name])){
+			$c_options = C('ACM:'.$name);
 			if(empty($c_options)){
 				$c_options = array();
 			}
 			$options = array_merge($c_options,$options);
-			self::$handlers[$type] = Server\ACM\Factory::getInstance($type,$options);
+			self::$handlers[$name] = Server\ACM\Factory::getInstance($name,$options);
 		}
-		return self::$handlers[$type];
+		return self::$handlers[$name];
 	}
 }
--- Koala/Server/Cache.php
+++ Koala/Server/Cache.php
@@ -1,26 +1,43 @@
 <?php
 /**
- * 缓存服务
+ * Koala - A PHP Framework For Web
+ *
+ * @package  Koala
+ * @author   Lunnlew <Lunnlew@gmail.com>
+ */
+/**
+ * 缓存服务类
+ * 
+ * @package  Koala
+ * @subpackage  Server
+ * @author    Lunnlew <Lunnlew@gmail.com>
  */
 class Cache{
   	/**
    	* 操作句柄数组
    	* @var array
+   	* @access protected
    	*/
-  	static protected $handlers = array();
-	public function __construct(){}
-	public static function factory($type='',$options=array()){
-		if(empty($type)||!is_string($type)){
-			$type = C('CACHE:DEFAULT','LAEMemcache');
+  	protected static $handlers = array();
+  	/**
+  	 * 访问控制实例化函数
+  	 * 
+  	 * @param  string $name    驱动名
+  	 * @param  array  $options 驱动构造参数
+  	 * @return object          驱动实例
+  	 */
+	public static function factory($name='',$options=array()){
+		if(empty($name)||!is_string($name)){
+			$name = C('CACHE:DEFAULT','LAEMemcache');
 		}
-		if(!isset(self::$handlers[$type])){
-			$c_options = C('Cache:'.$type);
+		if(!isset(self::$handlers[$name])){
+			$c_options = C('Cache:'.$name);
 			if(empty($c_options)){
 				$c_options = array();
 			}
 			$options = array_merge($c_options,$options);
-			self::$handlers[$type] = Server\Cache\Factory::getInstance($type,$options);
+			self::$handlers[$name] = Server\Cache\Factory::getInstance($name,$options);
 		}
-		return self::$handlers[$type];
+		return self::$handlers[$name];
 	}
 }
