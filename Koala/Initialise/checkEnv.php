0dbbff533b0da11c8d8b8b00cf7b22c1a13f0bf3
lunnlew <lunnlew@qq.com>
2014-04-13 19:47:57 +0800

    重新实现checkEnv.php

--- Koala/Initialise/checkEnv.php
+++ Koala/Initialise/checkEnv.php
@@ -1,20 +1,25 @@
 <?php
 //+++++++++++++运行模式及环境检查++++++++++++
-//-------------------信息注册--------------
-//php运行版本
-env::reg('PHP_VERSION',function($key){return PHP_VERSION;});
+//php版本最低需求
+env::reg('MIN_PHP_VERSION',function($key){return "5.3";});
+
 //应用引擎环境
-env::reg('APP_ENGINE',function($key){
-	//新浪SAE检测
-	if(defined('SAE_ACCESSKEY')){
-		define("APPENGINE","SAE");
-	}elseif(isset($_SERVER['HTTP_BAE_ENV_APPID'])){//BAE检测
-		define("APPENGINE","BAE");
-	}else{//本地应用环境
-	    define("APPENGINE","LAE");
-	}
-	return APPENGINE;
+env::check('APP_ENGINE',function($key){
+    //新浪SAE检测
+    if(defined('SAE_ACCESSKEY')){
+        define("APPENGINE","SAE");
+    }elseif(isset($_SERVER['HTTP_BAE_ENV_APPID'])){//BAE检测
+        define("APPENGINE","BAE");
+    }else{//本地应用环境
+        define("APPENGINE","LAE");
+    }
+    $result = array(
+            'require'=>'',
+            'current'=> APPENGINE
+            );
+    return $result;
 });
+
 //应用相对URL路径
 env::reg('APP_RELATIVE_URL',function($key){
 	if(RUNCLI)//cli $_SERVER['SERVER_NAME'] 无效
@@ -33,23 +38,83 @@ env::reg('APP_RELATIVE_URL',function($key){
 	return $pathname;
 });
 
-//php最低版本
-env::reg('MIN_PHP_VERSION',function($key){return "5.3";});
-env::reg("DEBUGLEVEL",function($key){
-		return defined('DEBUGLEVEL')?DEBUGLEVEL:C('DEBUGLEVEL',1);
-	});
-//--------------------信息检查---------------------
-env::check("PHP_VERSION",function($value,$key){
-	if(version_compare($value,env::get("MIN_".$key),"<")){
-		echo '当前PHP运行版本['.$value."]低于最低需求版本[".env::get("MIN_".$key)."]";
-		exit;
-	}
-	if(version_compare($value,"4.2.3","<=")){
-		//register_globals设置关闭
-		env::check("register_globals",function($key){
-			ini_set($key, 0);
-		});
-	}
+//--------------------运行环境检查---------------------
+env::check("PHP_OS",function($key){
+    $result = array(
+        'require'=>'不限制',
+        'current'=>PHP_OS
+        );
+    return $result;
+});
+env::check("PHP_VERSION",function($key){
+    $result = array(
+        'require'=>env::get("MIN_".$key),
+        'current'=>PHP_VERSION
+        );
+    if(version_compare(PHP_VERSION,env::get("MIN_".$key),"<")){
+        $result['msg'] = '当前PHP运行版本['.PHP_VERSION."]低于最低需求版本[".env::get("MIN_".$key)."]";
+        $result['state'] = 'error';
+    }else{
+        $result['msg'] = "";
+        $result['state'] = 'success';
+    }
+    if(version_compare(PHP_VERSION,"4.2.3","<=")){
+        //register_globals设置关闭
+        env::check("register_globals",function($key){
+            ini_set($key, 0);
+        });
+    }
+    return $result;
+});
+env::check("PHP_RUNMODE",function($key){
+    $result = array(
+        'require'=>'不限制',
+        'current'=>php_sapi_name()
+        );
+    return $result;
+});
+env::check("PHP_UPLOADSIZE",function($key){
+    $result = array(
+            'require'=>'不限制',
+            'current'=>ini_get('upload_max_filesize'),
+            );
+    return $result;
+});
+env::check("PHP_MAXTIME",function($key){
+    $result = array(
+            'require'=>'不限制',
+            'current'=>ini_get('max_execution_time') . "秒",
+            );
+    return $result;
+});
+env::check("PHP_SPACE",function($key){
+    $result = array(
+            'require'=>'不限制',
+            'current'=>round((@disk_free_space(".") / (1024 * 1024)), 2) . 'M',
+            );
+    return $result;
+});
+env::check("PHP_SERVER_TIME",function($key){
+    $result = array(
+            'require'=>'',
+            'current'=> date("Y年n月j日 H:i:s"),
+            );
+    return $result;
+});
+env::check("BEIJING_TIME",function($key){
+    $result = array(
+            'require'=>'',
+            'current'=> gmdate("Y年n月j日 H:i:s", time() + 8 * 3600)
+            );
+    return $result;
+});
+//--------------------函数、类依赖检查---------------------
+env::check("file_get_contents",function($key){
+    $result = array(
+            'require'=>true,
+            'current'=>function_exists('file_get_contents'),
+            );
+    return $result;
 });
 /**
  * 环境信息
@@ -76,7 +141,10 @@ class env{
         if(isset(static::$items[$key])){
             return $check(static::$items[$key],$key);
         }
-        return $check($key);
+        $value = $check($key);
+        if(!empty($value)||isset($value)){
+            static::$items[$key] = $value;
+        }
     }
     /**
      * 获取
