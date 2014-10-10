0e9de93e2df880933ed76e1bcf006a3a9dca0113
lunnlew <lunnlew@qq.com>
2014-10-10 16:07:05 +0800

    修正RSS生成器处理特殊字符的不合理问题

--- Koala/Helper/SitemapWriter/SimpleXMLElement.php
+++ Koala/Helper/SitemapWriter/SimpleXMLElement.php
@@ -11,7 +11,7 @@ namespace Koala\Helper\SitemapWriter;
 class SimpleXMLElement extends \SimpleXMLElement {
 	public function addChild($name, $value = null, $namespace = null) {
 		if ($value !== null and is_string($value) === true) {
-			$value = str_replace(array('&', '<', '>'), array('&amp;', '&lt;', '&gt;'), $value);
+			$value = str_replace(array('&'), array('&amp;'), $value);
 		}
 		return parent::addChild($name, $value, $namespace);
 	}
